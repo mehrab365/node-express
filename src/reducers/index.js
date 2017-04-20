b0f889160b16571f24d4f2c34f2a2aadd5d90d2b
markievollick <markievollick@gmail.com>
2017-04-20 11:38:21 -0500

    first commit modules 1-8 cory house

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import {combineReducers} from 'redux';
+import courses from './courseReducer';
+
+const rootReducer = combineReducers({
+  courses //shorthand property name
+});
+
+export default rootReducer;

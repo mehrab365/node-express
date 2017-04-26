d06ed76e00954b7d5b85abe46cf1d0318f3ea43f
markievollick <markievollick@gmail.com>
2017-04-26 12:34:05 -0500

    testing added

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import {combineReducers} from 'redux';
 import courses from './courseReducer';
+import authors from './authorReducer';
+import ajaxCallsInProgress from './ajaxStatusReducer';
 
 const rootReducer = combineReducers({
-  courses //shorthand property name
+  courses, //shorthand property name
+  authors,
+  ajaxCallsInProgress
 });
 
 export default rootReducer;
